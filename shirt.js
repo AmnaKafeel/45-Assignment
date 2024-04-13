188dd4aaef4cc9d69b564b436683c30d8dd9a003
Amna Kafeel <160414049+AmnaKafeel@users.noreply.github.com>
2024-04-13 14:05:06 +0500

    Add files via upload
    Assignment 36: T-Shirt

---
+++ shirt.js
@@ -0,0 +1,7 @@
+"use strict";
+function make_shirt(size, text) {
+    console.log(`Ceating a ${size} Shirt with the message: ${text}`);
+}
+make_shirt('Large', 'Hello Success');
+make_shirt('Medium', 'Focus On Your Carrer');
+make_shirt('Small', 'Hello World');
---
+++ shirt.ts
@@ -0,0 +1,9 @@
+
+function make_shirt(size : string, text : string){
+
+    console.log(`Ceating a ${size} Shirt with the message: ${text}`);
+}
+
+make_shirt('Large', 'Hello Success');
+make_shirt('Medium', 'Focus On Your Carrer');
+make_shirt('Small', 'Hello World');
