c5f73177d5d427fc02a7881b6f411c986d9830cd
Amna Kafeel <160414049+AmnaKafeel@users.noreply.github.com>
2024-04-13 14:09:01 +0500

    Add files via upload
    Assignment 39: City Names

---
+++ cityname.ts
@@ -0,0 +1,11 @@
+
+function city_country(city: string, country: string){
+
+    return `${city}, ${country}`;
+
+}
+console.log(city_country('Karachi', 'Pakistan'));
+
+console.log(city_country('Madina', 'Saudia Arabia'));
+
+console.log(city_country('Paris', 'France'));
