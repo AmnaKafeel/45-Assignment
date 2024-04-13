6a005e153cd899b2fa57b7eac8d635d468da9910
Amna Kafeel <160414049+AmnaKafeel@users.noreply.github.com>
2024-04-13 13:51:51 +0500

    Add files via upload
    Assignment 25: Alien Colors #1

---
+++ alien1.js
@@ -0,0 +1,12 @@
+"use strict";
+//create a varible called alien_color
+let alien_color = "red";
+//write an statement to test whether the alien color is green.If is it, print a massage that "the player just earned 5 points".
+if (alien_color === "red") {
+    console.log("Player earned Just 5 Points !");
+}
+//Write one version of this program that passes the if test and another that fail. (The Version that fail will have no output).
+alien_color = 'Yellow';
+if (alien_color === "red") {
+    console.log("Player earned Just 5 Points !");
+}
