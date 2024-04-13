bc3683b87b82135e865285f26710721704e8f065
Amna Kafeel <160414049+AmnaKafeel@users.noreply.github.com>
2024-04-13 13:55:02 +0500

    Add files via upload
    Assignment 29: Favorite Fruit: Make a array of your favorite fruits

---
+++ fruits.ts
@@ -0,0 +1,16 @@
+let favorite_fruits : string[]=["Apple", "Mango", "Strawberry"];
+if(favorite_fruits.includes("Mango")){
+    console.log("I really Like Mango !");
+}
+if(favorite_fruits.includes("Apple")){
+    console.log("I really Like Apple !");
+}
+if(favorite_fruits.includes("Strawberry")){
+    console.log("I really Like Stawberries !");
+}
+if(favorite_fruits.includes("Orange")){
+    console.log("I really Like Orange !");
+}
+if(favorite_fruits.includes("Cherry")){
+    console.log("I really Like Cherries !");
+}
