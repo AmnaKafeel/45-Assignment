9acfe9c93e4fe13e74215491ec3be5eb7735fd1b
Amna Kafeel <160414049+AmnaKafeel@users.noreply.github.com>
2024-04-13 13:57:30 +0500

    Add files via upload
    Assignment 31: No Users

---
+++ no_user.js
@@ -0,0 +1,9 @@
+"use strict";
+let userName = ['Admin', 'Nabeel', 'Kafeel', 'Farhaan'];
+if (userName.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    userName = [];
+    console.log('All Users Name has been Removed. ' + userName.length);
+}
---
+++ no_user.ts
@@ -0,0 +1,8 @@
+let userName : string[]=['Admin', 'Nabeel', 'Kafeel', 'Farhaan'];
+
+if(userName.length === 0){
+    console.log("We need to find some users!");
+}else {
+    userName = [];
+    console.log('All Users Name has been Removed. ' + userName.length);
+}
