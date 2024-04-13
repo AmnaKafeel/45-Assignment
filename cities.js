8535cacbb089602c8f71526871e2daec1038bbb8
Amna Kafeel <160414049+AmnaKafeel@users.noreply.github.com>
2024-04-13 14:07:58 +0500

    Add files via upload
    Assignment 38: Cities: Write a function called describe city that accepts the name of a city and its country

---
+++ cities.js
@@ -0,0 +1,7 @@
+"use strict";
+function discribe_city(city, country = 'Default Country') {
+    console.log(`${city} is in ${country}.`);
+}
+discribe_city('Karachi', 'Pakistan');
+discribe_city('Mecca', 'Saudi Arabia');
+discribe_city('Paris');
---
+++ cities.ts
@@ -0,0 +1,11 @@
+
+function discribe_city(city:string, country:string= 'Default Country'){
+
+    console.log(`${city} is in ${country}.`);
+}
+
+discribe_city('Karachi', 'Pakistan');
+
+discribe_city('Mecca', 'Saudi Arabia');
+
+discribe_city('Paris');
