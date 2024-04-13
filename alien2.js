c3912e7c9afe5294731ddab86a68366d199c3254
Amna Kafeel <160414049+AmnaKafeel@users.noreply.github.com>
2024-04-13 13:52:35 +0500

    Add files via upload
    Assignment 26: Alien Colors #2

---
+++ alien2.js
@@ -0,0 +1,17 @@
+"use strict";
+//Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+let alienColor = 'Yellow';
+if (alienColor === 'Yellow') {
+    console.log("the player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log(" the player just earned 10 points.");
+}
+//• Write one version of this program that runs the if block and another that runs the else block.
+(alienColor = 'Red');
+if (alienColor === 'Yellow') {
+    console.log("the player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log(" the player just earned 10 points.");
+}
---
+++ alien2.ts
@@ -0,0 +1,20 @@
+//Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+
+let alienColor : string= 'Yellow';
+
+if(alienColor === 'Yellow'){
+    console.log("the player just earned 5 points for shooting the alien.");
+}
+else{
+    console.log(" the player just earned 10 points.")
+}
+
+//• Write one version of this program that runs the if block and another that runs the else block.
+(alienColor = 'Red');
+
+if(alienColor === 'Yellow'){
+    console.log("the player just earned 5 points for shooting the alien.");
+}
+else{
+    console.log(" the player just earned 10 points.");
+}
