87c7ba4cd980379be7853ef27fe227e9146759b7
Amna Kafeel <160414049+AmnaKafeel@users.noreply.github.com>
2024-04-13 13:50:07 +0500

    Add files via upload
    Assignment 23: Conditional Tests

---
+++ test.js
@@ -0,0 +1,11 @@
+"use strict";
+let car = "subaru";
+//Test no1
+console.log("Is Car === 'subaru'? I Predict True.");
+console.log(car === 'subaru');
+//Test no2
+console.log("Is Car == 'subaru'? I Predict True.");
+console.log(car == 'subaru');
+//Test no3
+console.log("Is Car != 'Honda'? I Predict True.");
+console.log(car != 'Honda');
---
+++ test.ts
@@ -0,0 +1,42 @@
+let car : string= "subaru" ;
+
+
+//Test no1
+console.log("Is Car === 'subaru'? I Predict True.");
+console.log(car === 'subaru'); 
+
+//Test no2
+console.log("Is Car == 'subaru'? I Predict True.");
+console.log(car == 'subaru'); 
+
+//Test no3
+console.log("Is Car != 'Honda'? I Predict True.");
+console.log(car != 'Honda'); 
+
+//Test no4
+console.log("Is Car !== 'ford'? I Predict True.");
+console.log(car !== 'ford'); 
+
+//Test no5
+console.log("Is Car.Upper case == 'SUBARU'? I Predict True.");
+console.log(car.toUpperCase() == 'SUBARU'); 
+
+//Test no6
+console.log("Is Car == 'SUBARU'? I Predict False.");
+console.log(car == 'SUBARU'); 
+
+//Test no7
+console.log("Is Car === 'SUBARU'? I Predict True.");
+console.log(car === 'SUBARU'); 
+
+//Test no8
+console.log("Is Car === 'Train'? I Predict True.");
+console.log(car === 'Train'); 
+
+//Test no9
+console.log("Is Car === 'Bus'? I Predict True.");
+console.log(car === 'Bus');
+
+//Test no10
+console.log("Is Car === 'Cycle'? I Predict True.");
+console.log(car === 'Cycle');
